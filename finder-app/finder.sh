b76556e44b0fb346704c9b9c1c0d59ab7411f798
Jorge Santos <dossantosjorgehenrique@gmail.com>
2024-11-29 14:17:19 -0300

    Add linux image

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh 
 
 if [ "$#" -lt 2 ]; then
     echo "No enough args were provided."
@@ -16,11 +16,18 @@ line_count=0
 
 
 
-while IFS=":" read -r file number; do
+grep_output=$(grep -R "$1" -e "$2" -c)
+file_count=0
+line_count=0
+
+IFS=$'\n' # Set IFS to handle multiline input
+for line in $grep_output; do
+    file=$(echo "$line" | cut -d ':' -f 1)
+    number=$(echo "$line" | cut -d ':' -f 2)
     if [ "$number" -ne 0 ]; then
         file_count=$((file_count + 1))
-        line_count=$((line_count + "$number"))
+        line_count=$((line_count + number))
     fi
-done < <(grep -R "$1" -e "$2" -c)
+done
 
 echo  "The number of files are $file_count and the number of matching lines are $line_count" 
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -5,13 +5,14 @@
 set -e
 set -u
 
-OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+
+OUTDIR=/tmp/aeld
 
 if [ $# -lt 1 ]
 then
@@ -22,6 +23,7 @@ else
 fi
 
 mkdir -p ${OUTDIR}
+if [ $? != 0 ]; then echo "ERROR"; exit; fi
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
@@ -35,7 +37,23 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    if [ $? != 0 ]; then echo "ERROR"; exit; fi
+
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    if [ $? != 0 ]; then echo "ERROR"; exit; fi
+
+    make -j4 ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} all
+    if [ $? != 0 ]; then echo "ERROR"; exit; fi
+
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} modules
+    if [ $? != 0 ]; then echo "ERROR"; exit; fi
+
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    if [ $? != 0 ]; then echo "ERROR"; exit; fi
 fi
+
+cp -r "${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image" "${OUTDIR}"
 
 echo "Adding the Image in outdir"
 
@@ -49,6 +67,22 @@ fi
 
 # TODO: Create necessary base directories
 
+mkdir rootfs
+if [ $? != 0 ]; then echo "ERROR"; exit; fi
+
+cd rootfs
+
+mkdir -p bin dev etc home  lib lib64 proc sbin sys tmp usr var
+if [ $? != 0 ]; then echo "ERROR"; exit; fi
+
+mkdir -p usr/bin usr/lib usr/sbin
+if [ $? != 0 ]; then echo "ERROR"; exit; fi
+
+mkdir -p var/log
+if [ $? != 0 ]; then echo "ERROR"; exit; fi
+
+
+
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
@@ -56,25 +90,94 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    if [ $? != 0 ]; then echo "ERROR"; exit; fi
+
+    make defconfig
+    if [ $? != 0 ]; then echo "ERROR"; exit; fi
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+if [ $? != 0 ]; then echo "ERROR"; exit; fi
 
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}  CONFIG_PREFIX="${OUTDIR}/rootfs" install
+if [ $? != 0 ]; then echo "ERROR"; exit; fi
+
 
 # TODO: Add library dependencies to rootfs
 
+echo "Library dependencies"
+
+SYSROOT_PATH=$(${CROSS_COMPILE}gcc -print-sysroot)
+
+PROGRAM_INTERPRETER=$(${CROSS_COMPILE}readelf -l "${OUTDIR}/rootfs/bin/busybox"|   sed -n 's/.*Requesting program interpreter: \(.*\)/\1/p' | xargs basename | tr -d '[]')
+
+echo ${PROGRAM_INTERPRETER}
+
+PROGRAM_INTERPRETER_PATH=$(find "${SYSROOT_PATH}" -name "${PROGRAM_INTERPRETER}")
+
+echo ${PROGRAM_INTERPRETER_PATH}
+
+cp "$PROGRAM_INTERPRETER_PATH" "${OUTDIR}/rootfs/lib/"
+
+mapfile -t SHARED_LIBRARIES < <(${CROSS_COMPILE}readelf -d "${OUTDIR}/rootfs/bin/busybox"| awk '/NEEDED/ {print $5}' | tr -d '[]')
+
+echo 
+
+# Loop through each library in SHARED_LIBRARIES
+for lib in "${SHARED_LIBRARIES[@]}"; do
+    echo "Searching for $lib in $SYSROOT_PATH..."
+    
+    # Find the library in SYSROOT_PATH
+    lib_path=$(find "$SYSROOT_PATH" -name "$lib" | head -n 1)
+    
+    # Check if the library was found
+    if [[ -n "$lib_path" ]]; then
+        echo "Found $lib at $lib_path"
+        
+        # Copy the library to the output directory
+        cp "$lib_path" "$OUTDIR/rootfs/lib64/"
+        echo "Copied $lib to $OUTDIR/rootfs/lib64/"
+    else
+        echo "Library $lib not found in $SYSROOT_PATH!"
+    fi
+done
+
 # TODO: Make device nodes
+cd "${OUTDIR}/rootfs"
+
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
+
 
 # TODO: Clean and build the writer utility
+cd "$FINDER_APP_DIR"
+make clean 
+make CROSS_COMPILE=${CROSS_COMPILE} 
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp writer "${OUTDIR}/rootfs/home/"
+
+cp finder.sh "${OUTDIR}/rootfs/home/"
+mkdir -p "${OUTDIR}/rootfs/home/conf"
+cp conf/username.txt "${OUTDIR}/rootfs/home/conf/"
+cp conf/assignment.txt "${OUTDIR}/rootfs/home/conf/"
+cp finder-test.sh "${OUTDIR}/rootfs/home/"
+
+cp autorun-qemu.sh "${OUTDIR}/rootfs/home/"
 
 # TODO: Chown the root directory
+cd "${OUTDIR}/rootfs"
 
+sudo chown -R root:root *
 # TODO: Create initramfs.cpio.gz
+
+find  . | cpio  -H newc -ov --owner root:root > "${OUTDIR}/initramfs.cpio"
+
+cd "${OUTDIR}"
+
+gzip -f initramfs.cpio
